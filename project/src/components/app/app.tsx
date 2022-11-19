0d08922968e7cc5c97755fc1556d5822b752d598
RomanSteele <romanzelenenko@yandex.ru>
2022-11-19 19:05:22 +0100

    Adds pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,36 @@
+//import { Route, Routes } from 'react-router-dom';
+//import { AppRoute } from '../../const';
+//import FavoritesScreen from '../../pages/favorites-screen';
+//import LoginScreen from '../../pages/login-screen';
+import MainScreen from '../../pages/main-screen';
+//import RoomScreen from '../../pages/room-screen';
+
 function App(): JSX.Element {
-  return <p>Hello, world!!</p>;
+  return (
+    <MainScreen />
+  );
 }
 
 export default App;
+
+/*<Routes>
+      <Route
+        path={AppRoute.Main}
+        element={<MainScreen />}
+      />
+      <Route
+        path={AppRoute.Login}
+        element={
+          <LoginScreen />
+        }
+      />
+      <Route
+        path={AppRoute.Property}
+        element={<RoomScreen />}
+      />
+      <Route
+        path={AppRoute.Favorites}
+        element={<FavoritesScreen/>}
+      />
+    </Routes>
+    */
---
+++ project/src/const.ts
@@ -0,0 +1,6 @@
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Property = '/property',
+  Favorites = '/favorites',
+}
